b0d81ece5ce1943570cf9ee7611f068dad3e0c39
Ramazan Sancar <ramazansancar4545@gmail.com>
2023-09-26 20:14:07 +0300

    fix: weather api url fixed.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -76,13 +76,13 @@
                 })
             });
             // weather api
-            axios.get("https://weatherapi.cyclic.app/weather/Ankara?units=metric&lang=tr https://api.openweathermap.org/data/2.5/forecast/daily?q=Izmir&units=metric&lang=tr&appid=a3f46c687f2144a15d0adc8b5d513af2")
+            axios.get("https://weatherapi.cyclic.app/weather/weekly/daily/Izmir?units=metric&lang=tr&count=5&api=a3f46c687f2144a15d0adc8b5d513af2")
             .then((response) => {
                 this.weatherData = response.data.list.splice(0, 5);
                 this.todayTemp = Math.round(response.data.list[0].temp.day);
                 this.$store.state.weatherData = response.data.list.splice(0, 5);
-            }).
-            catch(error => {
+            })
+            .catch(error => {
                 console.log(error);
             });
         }
